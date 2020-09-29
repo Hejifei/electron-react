8ea40e7291114a042a24cef56455b8e45da60f14
Jifei <evan@dhms.com.cn>
2020-09-29 16:05:41 +0800

    feat:build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// const { ipcRenderer } = require('electron')
+
+const ipcRenderer = window.require("electron").ipcRenderer;
+
+
+
+// 异步回复
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  console.log(`main Process says: ${arg}`) // prints "pong"
+})
+
+// 同步发送信息
+ipcRenderer.send('asynchronous-message', 'hi')
 
 function App() {
+  const sendMessage = () => {
+    console.log(`main process repeat: 
+      ${ipcRenderer.sendSync('synchronous-message', 'Hey, are you main process?')}
+    `) // prints "pong"
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, boy!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button label="close" onClick={sendMessage} >
+          call main process
+        </button>
       </header>
     </div>
   );
